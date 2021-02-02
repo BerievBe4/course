da875cb9cb93e79cc1ce868d971db3749e197754
Daniil Uskov <uskov.dan@mail.ru>
2021-02-02 10:48:49 +0300

    3

--- CourseWork/Form1.Designer.cs
+++ CourseWork/Form1.Designer.cs
@@ -32,35 +32,35 @@
             System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title2 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.label1 = new System.Windows.Forms.Label();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(64, 181);
+            this.button1.Location = new System.Drawing.Point(64, 102);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(128, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "Начать получение";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(64, 22);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(128, 23);
-            this.button2.TabIndex = 3;
-            this.button2.Text = "Получить порты";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // comboBox1
             // 
@@ -84,7 +84,7 @@
             this.chart1.ChartAreas.Add(chartArea1);
             legend1.Name = "Legend1";
             this.chart1.Legends.Add(legend1);
-            this.chart1.Location = new System.Drawing.Point(284, 12);
+            this.chart1.Location = new System.Drawing.Point(282, 39);
             this.chart1.Name = "chart1";
             series1.ChartArea = "ChartArea1";
             series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
@@ -92,7 +92,7 @@
             series1.Legend = "Legend1";
             series1.Name = "Series1";
             this.chart1.Series.Add(series1);
-            this.chart1.Size = new System.Drawing.Size(540, 300);
+            this.chart1.Size = new System.Drawing.Size(542, 264);
             this.chart1.TabIndex = 6;
             this.chart1.Text = "chart1";
             title1.Name = "Title";
@@ -105,20 +105,91 @@
             this.fileSystemWatcher1.EnableRaisingEvents = true;
             this.fileSystemWatcher1.SynchronizingObject = this;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(90, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Выберите порт";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(282, 325);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.IsVisibleInLegend = false;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(542, 264);
+            this.chart2.TabIndex = 8;
+            this.chart2.Text = "chart2";
+            title2.Name = "Title";
+            title2.Text = "1";
+            title2.Visible = false;
+            this.chart2.Titles.Add(title2);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(350, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "График значений";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(344, 306);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "График производной";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(64, 306);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(128, 20);
+            this.textBox1.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(61, 279);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(124, 13);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Введите время работы";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(678, 319);
+            this.ClientSize = new System.Drawing.Size(678, 601);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.chart1);
             this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.menuStrip1);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -127,11 +198,16 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
         private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
--- CourseWork/Form1.cs
+++ CourseWork/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.IO.Ports;
 using System.Threading;
 using System.IO;
+using System.Diagnostics;
 
 namespace CourseWork
 {
@@ -18,6 +19,24 @@ namespace CourseWork
         public Form1()
         {
             InitializeComponent();
+            comboBox1.Items.Clear();
+            // Получаем список COM портов доступных в системе
+            string[] portnames = SerialPort.GetPortNames();
+            // Проверяем есть ли доступные
+            if (portnames.Length == 0)
+            {
+                MessageBox.Show("COM PORT not found");
+            }
+            foreach (string portName in portnames)
+            {
+                //добавляем доступные COM порты в список           
+                comboBox1.Items.Add(portName);
+                Console.WriteLine(portnames.Length);
+                if (portnames[0] != null)
+                {
+                    comboBox1.SelectedItem = portnames[0];
+                }
+            }
 
         }
 
@@ -39,24 +58,6 @@ namespace CourseWork
 
         private void button2_Click(object sender, EventArgs e)
         {
-            comboBox1.Items.Clear();
-            // Получаем список COM портов доступных в системе
-            string[] portnames = SerialPort.GetPortNames();
-            // Проверяем есть ли доступные
-            if (portnames.Length == 0)
-            {
-                MessageBox.Show("COM PORT not found");
-            }
-            foreach (string portName in portnames)
-            {
-                //добавляем доступные COM порты в список           
-                comboBox1.Items.Add(portName);
-                Console.WriteLine(portnames.Length);
-                if (portnames[0] != null)
-                {
-                    comboBox1.SelectedItem = portnames[0];
-                }
-            }
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -65,18 +66,23 @@ namespace CourseWork
             {
                 isReading = !isReading;
                 isConnected = true;
+                long maxTime = Int32.MaxValue;
                 string selectedPort = comboBox1.GetItemText(comboBox1.SelectedItem);
                 serialPort.PortName = selectedPort;
                 serialPort.Open();
                 button1.Text = "Отсоединиться";
-                int y = 50;
+                bool first = true;
                 int i = 0;
                 string str;
                 int res;
+                long currentTime = 0;
                 bool write;
+                double prevRes = 0;
+                double floatres;
                 OpenFileDialog ofd = new OpenFileDialog();
 
                 ofd.Filter = "Text files(*.txt) | *.txt";
+                Stopwatch stopwatch = new Stopwatch();
 
                 if (ofd.ShowDialog() == DialogResult.OK)
                 {
@@ -96,30 +102,108 @@ namespace CourseWork
                 else
                     write = false;
                 string filename = ofd.FileName;
+                chart2.ChartAreas[0].AxisY.Maximum = 10;
+                chart2.ChartAreas[0].AxisY.Minimum = -10;
 
-                while (isReading)
+                if (!String.IsNullOrEmpty(textBox1.Text))
                 {
-                    str = serialPort.ReadLine();
-                    str = str.Replace(".00\r", "");
-                    res = Int32.Parse(str);
-                    if (write)
+                    maxTime = Int32.Parse(textBox1.Text);
+                }
+
+                while (isReading && (currentTime < maxTime))
+                {
+                    if (first)
                     {
-                        using (FileStream theFile = new FileStream(ofd.FileName, FileMode.Append, FileAccess.Write))
+                        str = serialPort.ReadExisting();
+                        stopwatch.Start();
+                        var n = str.IndexOf("\r");
+                        if (!string.IsNullOrEmpty(str) && n > 0)
                         {
-                            using (StreamWriter sw = new StreamWriter(theFile))
-
+                            str = str.Substring(0, n);
+                            if (!string.IsNullOrEmpty(str))
                             {
+                                res = Int32.Parse(str);
+                                floatres = res * 5;
+                                floatres /= 1024;
+                                floatres *= 1000 / 90 * 7.5;
+                                prevRes = floatres;
+                                Thread.Sleep(200);
+                                if (write)
+                                {
+                                    using (FileStream theFile = new FileStream(ofd.FileName, FileMode.Append, FileAccess.Write))
+                                    {
+                                        using (StreamWriter sw = new StreamWriter(theFile))
 
-                                sw.WriteLine(res);
+                                        {
 
+                                            sw.WriteLine(floatres);
+
+                                        }
+
+                                    }
+                                }
+                                chart1.Series[0].Points.AddXY(i++, floatres);
+                                chart1.ChartAreas[0].AxisX.Maximum = i + 25;
+                                chart1.ChartAreas[0].AxisX.Minimum = i - 25;
+                                chart1.Update();
                             }
-
                         }
+                        first = false;                      
                     }
-                    chart1.Series[0].Points.AddXY(i++, res);
-                    chart1.Update();
-                    chart1.ChartAreas[0].AxisX.Maximum = i + 25;
-                    chart1.ChartAreas[0].AxisX.Minimum = i - 25;
+                    else
+                    {
+                        stopwatch.Stop();
+                        var time = stopwatch.ElapsedMilliseconds;
+                        if (maxTime != Int32.MaxValue)
+                        {
+                            currentTime += stopwatch.ElapsedMilliseconds / 1000;
+                        }
+                        str = serialPort.ReadExisting();
+                        stopwatch.Start();
+                        var n = str.IndexOf("\r");
+                        if (!string.IsNullOrEmpty(str) && n > 0)
+                        {
+                            str = str.Substring(0, n);
+                            if (!string.IsNullOrEmpty(str))
+                            {
+                                res = Int32.Parse(str);
+                                floatres = res * 5;
+                                floatres /= 1024;
+                                floatres *= 1000 / 90 * 7.5;
+                                Thread.Sleep(200);
+                                if (write)
+                                {
+                                    using (FileStream theFile = new FileStream(ofd.FileName, FileMode.Append, FileAccess.Write))
+                                    {
+                                        using (StreamWriter sw = new StreamWriter(theFile))
+
+                                        {
+
+                                            sw.WriteLine(floatres);
+
+                                        }
+
+                                    }
+                                }
+                                chart1.Series[0].Points.AddXY(i++, floatres);
+                                chart1.ChartAreas[0].AxisX.Maximum = i + 25;
+                                chart1.ChartAreas[0].AxisX.Minimum = i - 25;
+                                chart1.Update();
+
+
+
+
+                                chart2.Series[0].Points.AddXY(i++, (floatres - prevRes) / time);
+                                chart2.ChartAreas[0].AxisX.Maximum = i + 25;
+                                chart2.ChartAreas[0].AxisX.Minimum = i - 25;
+                                chart2.Update();
+
+                                prevRes = floatres;
+                            }
+                        }
+                            
+                        first = false;
+                    }
                 }
 
                 
